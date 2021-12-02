805e3856922fd14185de830977a3f350399dc37b
Florent <florent.nolot@univ-reims.fr>
2021-12-02 11:27:26 +0100

    Solved error in InstanceLogMessage

--- Message/InstanceLogMessage.php
+++ Message/InstanceLogMessage.php
@@ -19,7 +19,7 @@ class InstanceLogMessage
     const SCOPE_PUBLIC = "public";
     const SCOPE_PRIVATE = "private";
 
-    public function __construct(string $uuid, string $content, string $type, string $scope = self::SCOPE_PRIVATE)
+    public function __construct(string $content, string $type, string $scope = self::SCOPE_PRIVATE)
     {
         if (!in_array($type, [self::TYPE_DEBUG, self::TYPE_INFO, self::TYPE_WARNING, self::TYPE_ERROR])) {
             throw new InvalidArgumentException('Wrong type provided');
@@ -27,26 +27,6 @@ class InstanceLogMessage
 
         $this->content = $content;
         $this->scope = $scope;
-        //$this->uuid = $uuid;
-        $this->uuid = sprintf(
-            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-            // 32 bits for "time_low"
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            // 16 bits for "time_mid"
-            mt_rand(0, 0xffff),
-            // 16 bits for "time_hi_and_version",
-            // four most significant bits holds version number 4
-            mt_rand(0, 0x0fff) | 0x4000,
-            // 16 bits, 8 bits for "clk_seq_hi_res",
-            // 8 bits for "clk_seq_low",
-            // two most significant bits holds zero and one for variant DCE1.1
-            mt_rand(0, 0x3fff)  | 0x8000,
-            // 48 bits for "node"
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff)
-        );
         $this->type = $type;
     }
 
